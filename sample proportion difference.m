b52eebfa133fcefdb9f2238c2871cac6cbe38131
Shaikh Mainul Islam <kyokushinwingchun@gmail.com>
2021-10-22 13:50:42 +0600

    Create sample proportion difference.m

---
+++ sample proportion difference.m
@@ -0,0 +1,28 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*area1raw = RandomVariate[BinomialDistribution[100, 0.5], 1000];*)
+
+
+(* ::Input:: *)
+(*Histogram[area1raw]*)
+
+
+(* ::Input:: *)
+(*area1prop = RandomVariate[BinomialDistribution[100, 0.5], 10000]/100;*)
+
+
+(* ::Input:: *)
+(*Histogram[area1prop]*)
+
+
+(* ::Input:: *)
+(*area2prop = RandomVariate[BinomialDistribution[100, 0.5], 10000]/100;*)
+
+
+(* ::Input:: *)
+(*Histogram[area2prop]*)
+
+
+(* ::Input:: *)
+(*Histogram[area1prop - area2prop]*)
